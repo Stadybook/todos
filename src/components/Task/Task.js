6dfbe56b39768a8d2abb81b2d6689bea351ab4b9
Anastasia Serebrennikova <Serebrennikovaav01@mail.ru>
2023-01-14 17:49:20 +0300

    fix editing tasks

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,55 +1,104 @@
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import './Task.css';
 
-function Task({
-    label,
-    date,
-    onDeleted,
-    id,
-    onToggleCompleted,
-    completed,
-    handlerClick,
-}) {
-    Task.propTypes = {
-        id: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        onToggleCompleted: PropTypes.func.isRequired,
-        onDeleted: PropTypes.func.isRequired,
-    };
-    let classNames = 'description';
-
-    if (completed) {
-        classNames += ' completed';
+export default class Task extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            labelState: '',
+            edit: false,
+        };
     }
 
-    const result = formatDistanceToNow(date, { includeSeconds: true });
+    onLabelChange = (e) => {
+        this.setState({
+            labelState: e.target.value,
+        });
+    };
 
-    return (
-        <div className='view'>
-            <input
-                id={id}
-                className='toggle'
-                type='checkbox'
-                onChange={onToggleCompleted}
-                checked={completed}
-            />
-            <label htmlFor={id}>
-                <span className={classNames}>{label}</span>
-                <span className='created'>created {result}</span>
-            </label>
-            <button
-                type='button'
-                className='icon icon-edit float-right'
-                onClick={handlerClick}
-            />
-            <button
-                type='button'
-                className='icon icon-destroy float-right'
-                onClick={onDeleted}
-            />
-        </div>
-    );
+    onSubmit = (e) => {
+        const { labelState } = this.state;
+        const { onChangeName } = this.props;
+        e.preventDefault();
+        onChangeName(e.target.id, labelState);
+        this.setState({
+            labelState: '',
+            edit: false,
+        });
+    };
+
+    handlerClick = () => {
+        const newEdit = true;
+        this.setState({
+            edit: newEdit,
+        });
+    };
+
+    render() {
+        const { label, date, onDeleted, id, onToggleCompleted, completed } =
+            this.props;
+        let classNames = 'description';
+
+        if (completed) {
+            classNames += ' completed';
+        }
+
+        const { labelState } = this.state;
+        const { edit } = this.state;
+        const clazz = edit ? 'editing' : '';
+
+        const result = formatDistanceToNow(date, { includeSeconds: true });
+
+        return (
+            <li className={clazz}>
+                <form className='' onSubmit={this.onSubmit} id={id}>
+                    <input
+                        type='text'
+                        className='edit'
+                        placeholder='Editing task'
+                        onChange={this.onLabelChange}
+                        value={labelState}
+                    />
+                </form>
+                <div className='view'>
+                    <input
+                        id={id.toString()}
+                        className='toggle'
+                        type='checkbox'
+                        onChange={onToggleCompleted}
+                        checked={completed}
+                    />
+                    <label htmlFor={id}>
+                        <span
+                            className={classNames}
+                            onClick={onToggleCompleted}
+                            aria-hidden='true'
+                        >
+                            {label}
+                        </span>
+                        <span className='created'>created {result}</span>
+                    </label>
+                    <button
+                        type='button'
+                        className='icon icon-edit float-right'
+                        onClick={this.handlerClick}
+                    />
+                    <button
+                        type='button'
+                        className='icon icon-destroy float-right'
+                        onClick={onDeleted}
+                    />
+                </div>
+            </li>
+        );
+    }
 }
 
-export default Task;
+Task.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onToggleCompleted: PropTypes.func.isRequired,
+    onDeleted: PropTypes.func.isRequired,
+};
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,92 +1,33 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
 import Task from '../Task';
 
 import './TaskList.css';
 
-export default class TaskList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            label: '',
-            edit: false,
-        };
-
-        this.onLabelChange = this.onLabelChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    handlerClick = () => {
-        const { edit } = this.state;
-        const newEdit = !edit;
-        this.setState({
-            edit: newEdit,
-        });
+export default function TaskList({
+    todos,
+    onDeleted,
+    onToggleCompleted,
+    onChangeName,
+}) {
+    TaskList.propTypes = {
+        onToggleCompleted: PropTypes.func.isRequired,
+        onDeleted: PropTypes.func.isRequired,
+        onChangeName: PropTypes.func.isRequired,
     };
+    const elements = todos.map((item) => {
+        const { id } = item;
 
-    onLabelChange = (e) => {
-        this.setState({
-            label: e.target.value,
-        });
-    };
+        return (
+            <Task
+                {...item}
+                key={id}
+                onDeleted={() => onDeleted(id)}
+                onToggleCompleted={() => onToggleCompleted(id)}
+                onChangeName={(ids, text) => onChangeName(ids, text)}
+            />
+        );
+    });
 
-    onSubmit = (e) => {
-        const { onChangeName } = this.props;
-        const { label } = this.state;
-        e.preventDefault();
-        onChangeName(e.target.id, label);
-        this.setState({
-            label: '',
-            edit: false,
-        });
-    };
-
-    render() {
-        const { todos, onDeleted, onToggleCompleted } = this.props;
-
-        const elements = todos.map((item) => {
-            const { id } = item;
-            const { edit } = this.state;
-            const { label } = this.state;
-
-            const input = (
-                <form className='' onSubmit={this.onSubmit} id={id}>
-                    <input
-                        type='text'
-                        className='edit'
-                        placeholder='Editing task'
-                        onChange={this.onLabelChange}
-                        value={label}
-                    />
-                </form>
-            );
-
-            let clazz = '';
-            if (edit) {
-                clazz = 'editing';
-            }
-
-            const el = edit ? input : null;
-            return (
-                <li key={id} className={clazz}>
-                    <Task
-                        {...item}
-                        onDeleted={() => onDeleted(id)}
-                        onToggleCompleted={() => onToggleCompleted(id)}
-                        handlerClick={this.handlerClick}
-                    />
-                    {el}
-                </li>
-            );
-        });
-
-        return <ul className='todo-list'>{elements}</ul>;
-    }
+    return <ul className='todo-list'>{elements}</ul>;
 }
-
-TaskList.propTypes = {
-
-    onToggleCompleted: PropTypes.func.isRequired,
-    onDeleted: PropTypes.func.isRequired,
-};
