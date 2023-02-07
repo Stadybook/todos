50599a3e11bdcd94c6a55bb98acd9874f361c7f2
Anastasia Serebrennikova <Serebrennikovaav01@mail.ru>
2023-02-07 10:53:15 +0900

    init new branch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
     return (
         <header className='header'>
-            <h1>Todos</h1>
+            <h1>todos_hooks</h1>
         </header>
     );
 }
